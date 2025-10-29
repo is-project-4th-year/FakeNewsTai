8a2fad1fcca6f56879197118f0c47e24a9426556
Tevin-O <tevin.omondi@strathmore.edu>
2025-10-29 21:53:48 +0300

    feat: add Firebase auth + perfect TAIEYE logo + full app

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/tevin_senator/Downloads/FakeNewsIteration4-main/.venv"
+set -gx VIRTUAL_ENV "/Users/tevin_senator/Desktop/FakeNewsTai/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- .venv/lib/python3.9/site-packages/cloudpathlib/__init__.py
+++ .venv/lib/python3.9/site-packages/cloudpathlib/__init__.py
@@ -1,12 +1,16 @@
+import os
 import sys
 
 from .anypath import AnyPath
 from .azure.azblobclient import AzureBlobClient
 from .azure.azblobpath import AzureBlobPath
 from .cloudpath import CloudPath, implementation_registry
-from .s3.s3client import S3Client
-from .gs.gspath import GSPath
+from .patches import patch_open, patch_os_functions, patch_glob, patch_all_builtins
 from .gs.gsclient import GSClient
+from .gs.gspath import GSPath
+from .http.httpclient import HttpClient, HttpsClient
+from .http.httppath import HttpPath, HttpsPath
+from .s3.s3client import S3Client
 from .s3.s3path import S3Path
 
 
@@ -27,6 +31,27 @@ __all__ = [
     "implementation_registry",
     "GSClient",
     "GSPath",
+    "HttpClient",
+    "HttpsClient",
+    "HttpPath",
+    "HttpsPath",
+    "patch_open",
+    "patch_glob",
+    "patch_os_functions",
+    "patch_all_builtins",
     "S3Client",
     "S3Path",
 ]
+
+
+if bool(os.environ.get("CLOUDPATHLIB_PATCH_OPEN", "")):
+    patch_open()
+
+if bool(os.environ.get("CLOUDPATHLIB_PATCH_OS", "")):
+    patch_os_functions()
+
+if bool(os.environ.get("CLOUDPATHLIB_PATCH_GLOB", "")):
+    patch_glob()
+
+if bool(os.environ.get("CLOUDPATHLIB_PATCH_ALL", "")):
+    patch_all_builtins()
